6955d128b3507d1fea949b688df8e94b768219fc
NikitaMalofeev <Front.Work3@gmail.com>
2023-04-30 23:30:56 +0300

    добавлена опция прохода сквозь стены, голова все еще с ошибкой

--- js/snake.js
+++ js/snake.js
@@ -17,19 +17,46 @@ const moveSnake = () => {
         newMovementSnake = { x: headSnake.x - 1, y: headSnake.y, d:direction, h: true
         }
         console.log(newMovementSnake)
-    } else if(direction === "up"){
+    } 
+    if(direction === "up"){
         newMovementSnake = { x: headSnake.x, y: headSnake.y - 1, d:direction, h: true
         }
-    } else if(direction === "right"){
+    } 
+    if(direction === "right"){
         newMovementSnake = { x: headSnake.x + 1, y: headSnake.y, d:direction, h: true
         }
-    } else if(direction === "down"){
+    } 
+    if(direction === "down"){
         newMovementSnake = { x: headSnake.x, y: headSnake.y + 1, d:direction, h: true
         }
     }
+
+    newMovementSnake = _setTeleportSnake(state.snake, newMovementSnake);
+
+
     // удаляем голову( первый элемент массива)
     state.snake.tail.shift();
     headSnake.h = false;
     // даем новое значение голове
     state.snake.tail.push(newMovementSnake);
+}
+
+const _setTeleportSnake = (snake, newHeadSnake) => {
+    const { direction } = snake;
+    const rowEdge = row - 1;
+    
+    if(newHeadSnake.x > rowEdge && direction === "right"){
+        return  { x: 0, y: newHeadSnake.y, d: newHeadSnake.d, h: newHeadSnake.h }
+    } 
+    if(newHeadSnake.x < 0 && direction === "left"){
+        return  { x: rowEdge, y: newHeadSnake.y, d: newHeadSnake.d, h: newHeadSnake.h }
+    }
+    if(newHeadSnake.y < 0 && direction === "up"){
+        return  { x: newHeadSnake.x, y: rowEdge, d: newHeadSnake.d, h: newHeadSnake.h }
+    }
+    if(newHeadSnake.y > rowEdge && direction === "down"){
+        return  { x: newHeadSnake.x, y: 0, d: newHeadSnake.d, h: newHeadSnake.h }
+    }
+
+    return  { x: newHeadSnake.x, y: newHeadSnake.y, d: newHeadSnake.d, h: newHeadSnake.h }
 }
